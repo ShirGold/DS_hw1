62acf7d3f95d81a475b347e09c2545ae65b7dc24
Alon <alonsamocha@gmail.com>
2021-04-07 19:07:38 +0300

    Interface

--- CharLinkedListNodeImpl.java
+++ CharLinkedListNodeImpl.java
@@ -3,13 +3,20 @@ public class CharLinkedListNodeImpl implements ICharLinkedListNode{
     private char val;
     private ICharLinkedListNode next;
 
-    public CharLinkedListImpl(char val) {
+    public CharLinkedListNodeImpl(char val) {
         this.val = val;
         this.next = null;
     }
 
-    @Override
     public char getChar() {
         return this.val;
+    }
+
+    public ICharLinkedListNode getNext() {
+        return this.next;
+    }
+
+    public void setNext(ICharLinkedListNode next) {
+        this.next = next;
     }
 }
