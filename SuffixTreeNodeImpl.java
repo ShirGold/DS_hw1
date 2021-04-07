5be0eadc85efe84d88f4aefc273880c590ea38bf
Shir Goldfarb <shir.goldfarb@gmail.com>
2021-04-07 20:12:58 +0300

    SuffixTreeNodeImpl_binarySearch
    completed

--- SuffixTreeNodeImpl.java
+++ SuffixTreeNodeImpl.java
@@ -1,3 +1,27 @@
-public class SuffixTreeNodeImpl {
-    
+public class SuffixTreeNodeImpl extends SuffixTreeNode {
+
+    public SuffixTreeNodeImpl(){super();}
+
+    public SuffixTreeNode search(char c){
+        return binarySearch(c, 0, (this.numOfChildren-1));
+    }
+
+    public SuffixTreeNode binarySearch(char target, int left, int right){
+        if (right < 0){
+            return null;
+        }
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == this.children[mid].chars.first.getChar()) {
+                return this.children[mid];
+            }
+            if (target > this.children[mid].chars.first.getChar()){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return null;
+    }
 }
